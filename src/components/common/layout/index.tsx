62a547a07f54f34aefdf8dc17412d900ccd6ace8
YouYou94 <ybh4115@gmail.com>
2024-02-12 19:48:07 +0900

    Refactor: Layout structural changes

--- src/components/common/layout/index.tsx
+++ src/components/common/layout/index.tsx
@@ -1,3 +1,25 @@
-export const Layout = () => {
-  return <></>;
+import styled from "styled-components";
+
+export const CommonLayout = () => {
+  return <Common></Common>;
 };
+
+export const RowLayout = () => {
+  return <Row></Row>;
+};
+
+export const ColumnLayout = () => {
+  return <Column></Column>;
+};
+
+const Common = styled.div`
+  display: flex;
+`;
+
+const Row = styled(Common)`
+  flex-direction: row;
+`;
+
+const Column = styled(Common)`
+  flex-direction: column;
+`;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,7 @@
 /* EXPORT COMPONENTS */
 
 /* COMMON */
-export { Layout } from "./common/layout";
+export { CommonLayout, RowLayout, ColumnLayout } from "./common/layout";
 export { SmallLabel, MiddleLabel, LargeLabel } from "./common/label";
 export { Button } from "./common/button";
 export { Logo } from "./common/logo";
