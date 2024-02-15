ff86cdea46d6c052006eda25c95f11b2434f47a9
YouYou94 <ybh4115@gmail.com>
2024-02-15 18:48:09 +0900

    init: Home / Top

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,3 +5,6 @@ export { CommonLayout, RowLayout, ColumnLayout } from "./common/layout";
 export { SmallLabel, CommonLabel, LargeLabel } from "./common/label";
 export { Button } from "./common/button";
 export { Logo } from "./common/logo";
+
+/* HOME */
+export { HomeTop } from "./home/top";
